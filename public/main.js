0b2b17aa1508dbde760438813e8bb7116b505d7f
Šimon Smejkal <simon.smejkal@seznam.cz>
2020-05-16 12:35:51 +0200

    WIP - createTable function

--- public/main.js
+++ public/main.js
@@ -1,26 +1,30 @@
+const createTable = data => {
+  const table = document.getElementById("table");
+  const tbHead = table.insertRow(0)
+  
+  data.forEach((line, i) => {
+    if (i === 0) {
+      Object.keys(line).forEach((key, i) => {
+        const tbCell = tbHead.insertCell(i)
+        tbCell.innerHTML = key
+      })
+    }
+    
+    const table = document.getElementById("table");
+    const row = table.insertRow(i + 1);
+    const cellsCount = Object.keys(line).length
+    
+    for(let i = 0; i < cellsCount; i++) {
+      const cell = row.insertCell(i)
+      cell.innerHTML = Object.values(line)[i]
+    }
+  })
+}
+
 const init = async () => {
-  console.log('javascript initialized')
-
-  // PROMISES
-
-  // fetch('https://jsonplaceholder.typicode.com/todos')
-  // .then(response => response.json())
-  // .then(data => {
-  //   for(let i = 0; i < data.length; i++) {
-  //     const line = data[i]
-  //   }
-  // })
-
-  // ASYNC/ AWAIT
-
   const response = await fetch('http://localhost:5000/api/users')
   const data = await response.json()
-  
-  data.forEach(line => {
-    console.log(line)
-  })
-  console.log(data)
-
+  createTable(data)
 }
 
 document.addEventListener('DOMContentLoaded', init)
